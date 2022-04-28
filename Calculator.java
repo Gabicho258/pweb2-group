7a5065d925b8f15ceed0e9197cd107993b77304c
Gabriel <gmamanican@unsa.edu.pe>
2022-04-28 17:10:08 -0500

    adding function to divide

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,9 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
 }
